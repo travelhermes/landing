2956aa615fd5e951278d6197c26ea94408fe9ffa
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2022-01-09 00:29:58 +0100

    Initial commit

---
+++ src/scripts/custom.js
@@ -0,0 +1,30 @@
+/* Change menu depending on scroll */
+const sections = document.querySelectorAll(".anchor");
+
+function switchMenuActive(hash) {
+  let currentLink = document.querySelector(".landing-nav").querySelector(".active");
+  if (currentLink) {
+    currentLink.classList.remove("active");
+    currentLink.classList.remove("font-weight-bold");
+    currentLink.classList.remove("text-white-dm");
+    currentLink.classList.remove("text-dark-lm");
+  }
+
+  let newLink = document.querySelector(".landing-nav").querySelector("#menu-" + hash);
+  newLink.classList.add("active");
+  newLink.classList.add("font-weight-bold");
+  newLink.classList.add("text-white-dm");
+  newLink.classList.add("text-dark-lm");
+  newLink.focus();
+}
+
+observer = new IntersectionObserver((entries) => {
+  if (entries.length == 1) {
+    var hash = entries[0].target.getAttribute("id");
+    switchMenuActive(hash);
+  }
+});
+
+sections.forEach((image) => {
+  observer.observe(image);
+});
