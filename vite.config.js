42cba527091ef30915af6665d6d69974e79aad32
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2022-02-20 12:12:14 +0100

    Fixed build command

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src/",
-  minify: 'esbuild'
+  minify: 'esbuild',
+  assetsInclude: ['**/*.jpg', '**/*.png'],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        charset: false
+      }
+    }
+  },
+  build: {
+    minify: false
+  }
 })
